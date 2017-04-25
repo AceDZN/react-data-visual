f11d3a78ff8b7f7917ab166aa5a72ed216d0d399
Alex Sindalovsky <design@acedzn.com>
2017-04-26 00:43:33 +0300

    added griddle-react for lat-long list fro, genomes API

--- source/components/map.js
+++ source/components/map.js
@@ -1,23 +1,65 @@
 import React from 'react';
+import Griddle, {RowDefinition,ColumnDefinition} from 'griddle-react';
 
 export default class MapApp extends React.Component {
+  constructor(props){
+    super(props);
+    this.mapClickEvent = this.mapClickEvent.bind(this);
+    this.state = {
+      cities : []
+    };
+
+  }
   mapClickEvent(e){
     let img = e.target,
       point = {x: e.offsetX, y: e.offsetY},
       latSize = img.height/180,
       lonSize = img.width/360,
-      lat, lon;
+      lat, lon,
+      range = 1;
 
     if(!point.x){
       point = { x: e.pageX - e.target.offsetLeft,
                 y: e.pageY - e.target.offsetTop};
     }
     lat = (point.y - (img.height/2)) / -latSize;
-    lon = (point.x - (img.width/2)) / -lonSize;
+    lon = (point.x - (img.width/2)) / lonSize;
 
     console.log("lat: "+lat+" | lon : "+lon);
+
+    let citiesUrl = `http://api.geonames.org/citiesJSON?formatted=true&north=${lat+range}&south=${lat-range}&east=${lon+range}&west=${lon-range}&lang=en&username=acedzn&style=full`;
+
+    var xhr = new XMLHttpRequest();
+       xhr.open('GET',citiesUrl);
+       xhr.onload = ()=>{
+         if(xhr.status === 200){
+           this.setState({cities: JSON.parse(xhr.responseText).geonames});
+           console.log(this.state.cities,"this.state");
+         } else {
+           console.log('Error');
+         }
+       };
+       xhr.send();
+
   }
   render(){
-    return <img onClick={this.mapClickEvent} src="https://cdn.pixabay.com/photo/2011/12/13/17/07/earth-11048_960_720.jpg" />;
+    //const CustomColumn = ({value}) => <span style={{ color: '#0000AA' }}>{value}</span>;
+    //const CustomHeading = ({title}) => <span style={{ color: '#AA0000' }}>{title}</span>;
+
+    return <div>
+      <img
+        onClick={this.mapClickEvent}
+        src="https://upload.wikimedia.org/wikipedia/commons/c/c4/Earthmap1000x500compac.jpg" />
+      <Griddle data={this.state.cities}>
+        <RowDefinition>
+          <ColumnDefinition id="name" />
+          <ColumnDefinition id="lat" />
+          <ColumnDefinition id="lng" />
+          <ColumnDefinition id="countrycode" />
+          <ColumnDefinition id="population" />
+          <ColumnDefinition id="wikipedia" />
+        </RowDefinition>
+      </Griddle>
+    </div>;
   }
 }

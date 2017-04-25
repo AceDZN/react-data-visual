d165786196be7378c494c0cdb51b1a764116e116
Alex Sindalovsky <design@acedzn.com>
2017-04-26 01:59:59 +0300

    chart data implementation

--- source/chart-client.js
+++ source/chart-client.js
@@ -6,13 +6,13 @@ const data = {
   type: 'bar',
   data: {
       labels: ['label1','label2','label3','label4','label5','label6'],
-      dataset: [{
+      datasets: [{
         label: 'Fun',
         data: [80,40,21,56,43,98],
         backgroundColor: 'rgba(255,0,0,.2)',
         borderWidth: 1
-      }],
-      dataset: [{
+      },
+      {
         label: 'nice',
         data: [20,52,10,90,100,13],
         backgroundColor: 'rgba(0,0,255,.2)',
--- source/components/chart.js
+++ source/components/chart.js
@@ -2,9 +2,15 @@ import React from 'react';
 import ChartJS from 'chart.js';
 
 export default class Chart extends React.Component{
+  componentDidMount(){
+    const ctx = this.refs['chart'].getContext('2d');
+    let {type, data, options} = this.props;
+    this.chart = new ChartJS(ctx, {type:type,data:data,options:options});
+  }
+
   render(){
-    return <div style={{width:900, height: 300, backgroundColor: 'blue'}}>
-      <canvas ></canvas>
+    return <div style={{width:600, height: 300, backgroundColor: 'white'}}>
+      <canvas ref="chart"></canvas>
     </div>;
   }
 }

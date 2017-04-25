d12748fe8272c6d51a25b18b969a039767d68a14
Alex Sindalovsky <design@acedzn.com>
2017-04-26 02:19:58 +0300

    starting csv stuff

--- source/chart-client.js
+++ source/chart-client.js
@@ -1,28 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Chart from './components/chart';
+import chartData from './data/chart-data.csv';
 
-const data = {
-  type: 'bar',
-  data: {
-      labels: ['label1','label2','label3','label4','label5','label6'],
-      datasets: [{
-        label: 'Fun',
-        data: [80,40,21,56,43,98],
-        backgroundColor: 'rgba(255,0,0,.2)',
-        borderWidth: 1
-      },
-      {
-        label: 'nice',
-        data: [20,52,10,90,100,13],
-        backgroundColor: 'rgba(0,0,255,.2)',
-        borderColor: 'rgba(0,0,255,.7)',
-        borderWidth: 1
-      }]
-  },
-  options: {
+let obj = chartData.shift(),
+   formatedData = {labels: [obj.name], datasets: []},
+   map: new Map();
 
+delete obj.name;
+for(let item in obj){
+  let ds = {label: item, data: [obj.item]};
+  formatedData.datasets.push(ds);
+  map[item] = ds;
+  console.log(ds);
+}
+
+chartData.forEach((obj, i, a)=>{
+  formatedData.labels.push(obj.name);
+  delete obj.name;
+  for(let item in obj){
+    map[item].data.push(obj[item])
   }
+});
+
+
+const data = formatedData;
 }
 
 ReactDOM.render(<Chart {...data} />, document.getElementById('react'));
